92cae2746fd18b2a26773163fb658e4525f661df
Sharepoint Admin <spadmin@sharepoint.local>
2016-09-27 19:22:01 -0300

    Adicionada a estrutura do projeto

---
+++ PocNavigation/PocNavigation/Itens.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocNavigation
+{
+    public class Item
+    {
+        private List<Item> itensFilhos;
+
+        public string Titulo { get; private set; }
+        public string URL { get; private set; }
+
+        public IEnumerable<Item> ItensFilhos { get { return itensFilhos; } }
+
+        public Item(string titulo, string url)
+        {
+            Titulo = titulo;
+            URL = url;
+            itensFilhos = new List<Item>();
+            
+        }
+
+        public void AdicionarItensFilhos(Item itens)
+        {
+            itensFilhos.Add(itens);
+
+            
+        }
+
+
+
+
+        
+
+
+
+    }
+}
